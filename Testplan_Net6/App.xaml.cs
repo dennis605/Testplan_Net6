10bd37bbde77eb2c2ce606d6858a3a7e6b385e3b
dennis605 <dennis605@web.de>
2023-05-02 21:46:53 +0200

    befor creating mvvm pattern -  done 4v8

---
+++ Testplan_Net6/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Testplan_Net6;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Testplan_Net6/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Testplan_Net6;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
